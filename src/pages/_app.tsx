e0e524fbfbb9d76156faa3771c916ab856f88a8b
Matheus Almeida <matheusalmeida016@gmail.com>
2021-06-16 01:54:39 -0400

    Add Search and User components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
+import { Search } from '../components/Search'
+
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Search />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,39 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Actions } from '../components/Actions'
+
+
 import styles from './Home.module.scss'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>GitHub App</title>
+        <title>GitSearch</title>
         <meta name="description" content="Search for githubers" />
         <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main className={styles.main}>
-        <h1>Hello, World!</h1>
+        <div className={styles.avatar}>
+          <Image 
+            width={200}
+            height={200}
+            src="https://github.com/mutheus.png" 
+          />
+          
+          <h2>Matheus Almeida</h2>
+        </div>
+        
+        <div className={styles.reposInfo}>
+          <ul>
+            <li>Repos: <span className={styles.numbers}>78</span></li>
+            <li>Followers: <span className={styles.numbers}>14</span></li>
+            <li>Following: <span className={styles.numbers}>35</span></li>
+          </ul>
+        </div>
+       
+        
       </main>
     </div>
   )
